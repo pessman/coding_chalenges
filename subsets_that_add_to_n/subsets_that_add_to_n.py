5eb4262198b819d49f1212a19fb6341dbff0337e
Peter Essman <pessman.dev@gmail.com>
2019-04-02 21:15:43 -0400

    added subsets that add to n module

---
+++ subsets_that_add_to_n/subsets_that_add_to_n.py
@@ -0,0 +1,21 @@
+
+
+def subsets_that_add_to_n(int_list, target):
+    if not isinstance(int_list, list):
+        raise TypeError("int_list parameter must be of type list")
+    if any([isinstance(integer, int) is False for integer in int_list]):
+        raise TypeError("all items in int_list parameter must be of type int")
+    if not isinstance(target, int):
+        raise TypeError("integer parameter must be of type int")
+
+    subsets = []
+    for index, integer in enumerate(int_list):
+        if target - integer > 0:
+            subs = subsets_that_add_to_n(int_list[index + 1:], target - integer)
+            for sub in subs:
+                integer_list = [integer]
+                subsets.append([*integer_list, *sub])
+        elif target - integer == 0:
+            subsets.append([integer])
+
+    return subsets
---
+++ subsets_that_add_to_n/subsets_that_add_to_n_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from subsets_that_add_to_n import subsets_that_add_to_n
+
+
+def test_invalid_list_type():
+    with pytest.raises(TypeError):
+        subsets = subsets_that_add_to_n(None, 20)
+
+def test_invalid_list_item_type():
+    with pytest.raises(TypeError):
+        subsets = subsets_that_add_to_n([1, 2, 3.3], 20)
+
+def test_invalid_target_type():
+    with pytest.raises(TypeError):
+        subsets = subsets_that_add_to_n([1,2,3], 3.3)
+
+def test_empty_list():
+    assert subsets_that_add_to_n([], 20) == []
+
+def test_no_subsets():
+    assert subsets_that_add_to_n([1, 2, 3], 100) == []
+
+def test_subsets():
+    assert subsets_that_add_to_n([2, 4, 6, 10], 16) == [[2, 4, 10], [6, 10]]
+    assert subsets_that_add_to_n([1, 2, 3, 4, 5], 10) == [[1, 2, 3, 4], [1, 4, 5], [2, 3, 5]]
+    assert subsets_that_add_to_n([2,3,2], 5) == [[2,3], [3,2]]
